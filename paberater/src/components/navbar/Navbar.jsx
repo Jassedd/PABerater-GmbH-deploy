72428383337b7c2c205fef36b62f8519248899b7
Jassed Martinez <jassedgmartinez@gmail.com>
2023-12-29 16:37:27 +0100

    feat(components)Navbar-added

---
+++ paberater/src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from "../../assets/img/Logos/LOGOORIGINAL.svg"
+import IntagramLogo from "../../assets/icons/logo_instagram.svg"
+import YoutubeLogo from "../../assets/icons/logo_youtube.svg"
+import FacebookLogo from "../../assets/icons/logo_facebook.svg"
+import "./Navbar.css"
+
+
+function NavbarHome() {
+  return (
+    <Navbar expand="lg" className="navbarHome">
+        <img src={Logo} alt="" className='LogoPaberater'/>
+      <Container className='NavContainer'>
+        <Navbar.Brand className='mediaLogo'>
+        <button className='btn-advisory'>Quiero saber más</button>
+        <img src={IntagramLogo} alt="instagram logo" className='socialMediaLogo' />
+        <img src={YoutubeLogo} alt="youtube logo" className='socialMediaLogo' />
+        <img src={FacebookLogo} alt="facebook logo" className='socialMediaLogo' />
+
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarHome;
--- paberater/src/router/Root.jsx
+++ paberater/src/router/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavbarHome from '../components/navbar/Navbar'
 
 function Root() {
   return (
     <>
-        <Outlet/>
+      <NavbarHome />
+      <Outlet />
     </>
   )
 }
