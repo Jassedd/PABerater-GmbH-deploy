7dfdb38a3a1eee7a65ce92aa397d2e6c65fa6eee
Jassed Martinez <jassedgmartinez@gmail.com>
2024-01-06 15:57:23 +0100

    fix(pages)modify-styles-somos-page

--- paberater/src/pages/whoWeAre/WhoWeAre.jsx
+++ paberater/src/pages/whoWeAre/WhoWeAre.jsx
@@ -1,10 +1,11 @@
+import "./WhoWeAre.css"
 import JorgeWe from "../../assets/img/jorge-pineda-web2.png"
 
-function whoWeAre() {
+function WhoWeAre() {
   return (
     <>
-      <section>
-        <h1>Somos</h1>
+      <section className="we-section">
+        <h1 style={{color: "#C8A75A"}}>Somos</h1>
         <p>
           una empresa de asesoría privada con sede en Alemania, nos
           especializamos en asesorar a profesionales cualificados para su
@@ -15,35 +16,34 @@ function whoWeAre() {
         </p>
       </section>
       <section className="section-figure">
-        <div className="figure">
-          <img src={JorgeWe} alt="" />
+        <div className="figure-we">
+          <img src={JorgeWe} alt="Jorge img" className="Jorge-we" />
         </div>
       </section>
       <section className="history">
-        <h1>Nuestra Historia</h1>
+        <h1 style={{color: "#C8A75A"}}>Nuestra Historia</h1>
         <p>
-          Jorge Joel Pineda Arcia, abogado egresado de la Universidad Gran
+          <span className="span-we">Jorge Joel Pineda Arcia,</span> abogado egresado de la Universidad Gran
           Mariscal de Ayacucho en Venezuela, en el año 2015 vivió la experiencia
           de emigrar y realizar su reconocimiento académico en Alemania y en
           2019 tuvo la idea de prestar el servicio de asesoría para inmigrantes
-          profesionales por lo que fundó la empresa PABerater UG. <br />
+          profesionales por lo que fundó la empresa <span className="span-we">PABerater UG.</span>  <br />
           <br />
           Debido al constante crecimiento de la industria en Alemania y al
           creciente número de plazas vacantes en el sector de la ingeniería, la
-          empresa PABerater estaba enfocada en este sector industrial y ahora
+          empresa <span className="span-we">PABerater</span> estaba enfocada en este sector industrial y ahora
           trabajamos con clientes en todos los ámbitos profesionales: médicos,
           psicopedagogos, ingenieros. La empresa se encuentra en continuo
-          crecimiento y gracias al esfuerzo y dedicación es ahora PABerater
-          GmbH.
+          crecimiento y gracias al esfuerzo y dedicación es ahora <span className="span-we">PABerater GmbH.</span>
         </p>
       </section>
       <section className="section-new-life">
-        <h2>Si yo pude, tú también puedes.</h2>
-        <h3>PABerater,</h3>
-        <h6>el compañero ideal para tu nueva vida en Alemania.</h6>
+        <h4 style={{color: "#25357a"}}>Si yo pude, tú también puedes.</h4>
+        <h3 style={{color: "white"}}>PABerater,</h3>
+        <h6 style={{color: "white"}}>el compañero ideal para tu nueva vida en Alemania.</h6>
       </section>
     </>
   );
 }
 
-export default whoWeAre;
+export default WhoWeAre;
