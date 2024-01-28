80570460b3b35c48be6a1f08c9c3d8252e9748df
Jassed <jassedgmartinez@gmail.com>
2024-01-28 23:18:51 +0100

    fix(admin)filter-users-added

--- paberater/src/admin/usersForms/UsersForms.jsx
+++ paberater/src/admin/usersForms/UsersForms.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from "../../../firebase/firebase";
 import Pagination from '../../components/pagination/Pagination';
 import ScrollToTop from '../../components/scrollToTop/ScrollToTop';
 import { Link } from 'react-router-dom';
 import "./UsersForms.css";
-import Accordion from 'react-bootstrap/Accordion';
+import { countries } from '../../components/countries/Countries';
+
 
 function UsersForms() {
   const [usersData, setUsersData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [filterSubject, setFilterSubject] = useState(null);
+  const [filterCountry, setFilterCountry] = useState(null);
+  const [filterNationality, setFilterNationality] = useState(null);
+  const [filtersChanged, setFiltersChanged] = useState(false);
 
   const pageSize = 12;
 
@@ -22,7 +27,21 @@ function UsersForms() {
 
     try {
       const usersCollection = collection(db, 'usersForms');
-      const querySnapshot = await getDocs(usersCollection);
+      let queryRef = query(usersCollection);
+
+      if (filterSubject) {
+        queryRef = query(usersCollection, where('subject', '==', filterSubject));
+      }
+      if (filterCountry) {
+        queryRef = query(usersCollection, where('country', '==', filterCountry));
+      }
+      if (filterNationality) {
+        queryRef = query(usersCollection, where('nacionality', '==', filterNationality));
+      }
+
+      const querySnapshot = await getDocs(queryRef);
+
+      console.log('Documentos filtrados:', querySnapshot.size);
 
       const usersArray = [];
       querySnapshot.forEach((doc) => {
@@ -34,6 +53,7 @@ function UsersForms() {
 
       setUsersData(usersArray.slice(startIndex, endIndex));
       setLoading(false);
+      setFiltersChanged(false);
     } catch (error) {
       console.error('Error fetching data:', error);
       setLoading(false);
@@ -46,7 +66,35 @@ function UsersForms() {
 
   useEffect(() => {
     fetchData(currentPage);
-  }, [currentPage]);
+  }, [currentPage, filterSubject, filterCountry, filterNationality]);
+
+  const handleFilterChange = (event, filterType) => {
+    const value = event.target.value;
+    switch (filterType) {
+      case 'subject':
+        setFilterSubject(value);
+        break;
+      case 'country':
+        setFilterCountry(value);
+        break;
+      case 'nationality':
+        setFilterNationality(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const applyFilters = () => {
+    fetchData(currentPage);
+  };
+  
+  const resetFilters = () => {
+    setFilterSubject(null);
+    setFilterCountry(null);
+    setFilterNationality(null);
+    fetchData(currentPage);
+  };
 
   if (loading) {
     return <div>Cargando...</div>;
@@ -56,24 +104,66 @@ function UsersForms() {
     <section>
       <ScrollToTop />
       <section className='Users'>
-        {usersData.map((post) => (
-          <div key={post.id} className="user-container" >
-            <Accordion defaultActiveKey="0">
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>{post.name}</Accordion.Header>
-                <Accordion.Body>
+      <div className="filter-controls">
+          <label>
+            Asunto:
+            <select onChange={(e) => handleFilterChange(e, 'subject')}>
+              <option value="">Todos</option>
+              <option value="Solicitud de asesoramiento">Solicitud de Asesoría</option>
+              <option value="Consulta">Consulta</option>
+            </select>
+          </label>
+          <label>
+            Nacionalidad:
+            <select onChange={(e) => handleFilterChange(e, 'nacionality')}>
+              <option value="">Todos</option>
+              {countries.map((nacionality) => (
+                <option key={nacionality.id} value={nacionality.id}>
+                  {nacionality.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label>
+            País de residencia:
+            <select onChange={(e) => handleFilterChange(e, 'country')}>
+              <option value="">Todos</option>
+              {countries.map((country) => (
+                <option key={country.id} value={country.id}>
+                  {country.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <button onClick={() => applyFilters()}>Aplicar Filtros</button>
+          <button onClick={() => resetFilters()}>Reestablecer Filtros</button>
+        </div>
+      {usersData.map((post, index) => (
+          <div key={post.id} className={`user-container ${index % 2 === 0 ? 'even' : 'odd'}`}>
+            <ul className='unorderListUsers' type="none">
+                <li className='usersList'>{post.name}
                   <div className="user-detail">
-                    <strong>Subject:</strong> {post.subject}
+                    <strong>Asunto:</strong> {post.subject}
+                  </div>
+                  <div className="user-detail">
+                    <strong>Profesión:</strong> {post.profession}
+                  </div>
+                  <div className="user-detail">
+                    <strong>Nacionalidad:</strong> {post.nacionality}
+                  </div>
+                  <div className="user-detail">
+                    <strong>País de residencia:</strong> {post.country}
                   </div>
                   <div className="user-detail">
                     <strong>Email:</strong> {post.email}
                   </div>
                   <div className="user-detail">
-                    <strong>Description:</strong> {post.description}
+                    <Link to="/descripcion">
+                      <strong className='dscription'>Descripción</strong>
+                    </Link>
                   </div>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
+                </li>
+            </ul>
           </div>
         ))}
       </section>
