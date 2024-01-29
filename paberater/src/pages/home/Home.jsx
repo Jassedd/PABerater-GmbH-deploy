889c0933d58b0a27f7869c8f792972fc69b08170
Diego Bordallo <bordallo.d@gmail.com>
2024-01-29 20:25:49 +0100

    fix(home/navbar/comments)fix styles

--- paberater/src/pages/home/Home.jsx
+++ paberater/src/pages/home/Home.jsx
@@ -17,31 +17,36 @@ const Home = () => {
   return (
     <main>
       <ScrollToTop />
-      <section className="init-section">
+      
+      <div className="container-img"></div>
         <div className="div-init">
-          <h1 className="title-init">
-            Asesoramos
-            <br />a <span className="resaltar">profesionales</span>
-            <br />
-            <span className="pequena">que</span>{" "}
-            <span className="pequena resaltar">desean</span>{" "}
-            <span className="resaltar">emigrar</span>
-            <br />
-            <span className="pequena">a </span>
-            <span className="resaltar">Alemania</span>
-          </h1>
-          <div className="btnAdvisoryHome"></div>
-          <BtnAdvisory />
+          
+            <img src={Jorge2} alt="Jorge" className="jorge-img2" />
+          
+          <div className="textbtn">
+            <h1 className="title-init">
+              Asesoramos
+              <br />a <span className="resaltar">profesionales</span>
+              <br />
+              <span className="pequena">que</span>{" "}
+              <span className="pequena resaltar">desean</span>{" "}
+              <span className="resaltar">emigrar</span>
+              <br />
+              <span className="pequena">a </span>
+              <span className="resaltar">Alemania</span>
+              <div className="btnAdvisoryHome"></div>
+              <BtnAdvisory />
+            </h1>
+          </div>
         </div>
-      </section>
-      <div className="container-img">
-        <img src={Jorge2} alt="Jorge" className="jorge-img2" />
-      </div>
-      <div className="container_background_img">
-        <img src={Berlin} alt="fondo" className="home_background" />
-      </div>
+      
+      
+     
       <section className="what-do-we-do">
+      <img src={Berlin} alt="fondo" className="home_background" />
+
         <div className="container-home">
+
           <div className="question-home">
             <h1 className="titleWeDo">¿Qué Hacemos?</h1>
           </div>
@@ -69,43 +74,44 @@ const Home = () => {
               />
             </svg>
 
-          <section className="Acompañamos">
-            <div className="empty">
-              <h2 className="titleStep">Acompañamos</h2>
-            </div>
-            <div className="empty">
-              <p>
-                paso a paso, antes y durante el proceso de la homologación o
-                reconocimiento de sus títulos profesionales.
-              </p>
-            </div>
-          </section>
-          <svg
-            width="50%"
-            height="auto"
-            className="first-arrow"
-            viewBox="0 0 147 48"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+            <section className="Acompañamos">
+              <div className="empty">
+                <h2 className="titleStep">Acompañamos</h2>
+              </div>
+              <div className="empty">
+                <p>
+                  paso a paso, antes y durante el proceso de la homologación o
+                  reconocimiento de sus títulos profesionales.
+                </p>
+              </div>
+            </section>
+            <svg
+              width="50%"
+              height="auto"
+              className="first-arrow"
+              viewBox="0 0 147 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-            <path
-              d="M141.333 3.5C141.333 4.97276 142.527 6.16667 144 6.16667C145.473 6.16667 146.667 4.97276 146.667 3.5C146.667 2.02724 145.473 0.833333 144 0.833333C142.527 0.833333 141.333 2.02724 141.333 3.5ZM144 24V24.5H144.5V24H144ZM3 24V23.5H2.5V24H3ZM0.333333 45C0.333333 46.4728 1.52724 47.6667 3 47.6667C4.47276 47.6667 5.66667 46.4728 5.66667 45C5.66667 43.5272 4.47276 42.3333 3 42.3333C1.52724 42.3333 0.333333 43.5272 0.333333 45ZM143.5 3.5V24H144.5V3.5H143.5ZM144 23.5H3V24.5H144V23.5ZM2.5 24V45H3.5V24H2.5Z"
-              fill="#FEEC8A"
+              <path
+                d="M141.333 3.5C141.333 4.97276 142.527 6.16667 144 6.16667C145.473 6.16667 146.667 4.97276 146.667 3.5C146.667 2.02724 145.473 0.833333 144 0.833333C142.527 0.833333 141.333 2.02724 141.333 3.5ZM144 24V24.5H144.5V24H144ZM3 24V23.5H2.5V24H3ZM0.333333 45C0.333333 46.4728 1.52724 47.6667 3 47.6667C4.47276 47.6667 5.66667 46.4728 5.66667 45C5.66667 43.5272 4.47276 42.3333 3 42.3333C1.52724 42.3333 0.333333 43.5272 0.333333 45ZM143.5 3.5V24H144.5V3.5H143.5ZM144 23.5H3V24.5H144V23.5ZM2.5 24V45H3.5V24H2.5Z"
+                fill="#FEEC8A"
               />
-          </svg>
+            </svg>
 
-          <section className="Gestionamos">
-            <div className="empty-column">
-              <h2 className="titleStep">Gestionamos</h2>
-            </div>
-            <div className="empty-column">
-              <p>
-                TODO. En PABerater somos conocedores del proceso porque pasamos
-                por ello y por eso nuestra misión es ayudarte proporcionándote
-                toda la información de una manera accesible y fácil de entender.
-              </p>
-            </div>
-          </section>
+            <section className="Gestionamos">
+              <div className="empty-column">
+                <h2 className="titleStep">Gestionamos</h2>
+              </div>
+              <div className="empty-column">
+                <p>
+                  TODO. En PABerater somos conocedores del proceso porque
+                  pasamos por ello y por eso nuestra misión es ayudarte
+                  proporcionándote toda la información de una manera accesible y
+                  fácil de entender.
+                </p>
+              </div>
+            </section>
           </div>
         </div>
         <article className="talent-home">
