01d6d380c4eb7239917053875ea35809db4ba64c
Jassed <jassedgmartinez@gmail.com>
2024-02-20 10:00:24 +0100

    feat(deploy)preparing-to-deploy

--- Server/utils/ramdomNumbers.js
+++
@@ -1,9 +0,0 @@
-export const generateRandomNumberString = (length) => {
-    const numbers = '0123456789';
-    let randomNumberString = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * numbers.length);
-      randomNumberString += numbers.charAt(randomIndex);
-    }
-    return randomNumberString;
-  };
