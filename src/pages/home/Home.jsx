9865f9df6816595d70341b3b41e8fdf04f190450
Emily <emily.artfree@gmail.com>
2024-02-20 19:10:52 +0100

    fix(feature-deploy)home details

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -122,7 +122,32 @@ const Home = () => {
       <Comments />
       <JoinUp />
       <FeedInstagram />
-      <QuestionForm />
+
+      <section className="questionsTitleFormHome">
+        <h2 className="titleQuestionFormHome">
+          De seguro tienes mucha dudas y nosotros estamos dispuestos a
+          responderlas todas
+        </h2>
+        <p className="question_subtitle_form">respuesta en 24 horas</p>
+      </section>
+
+      <section className="contactHome-section"> 
+        <div className="Q-form">
+        <QuestionForm />
+        </div>
+
+        <div className="pab_map">
+        <iframe
+          className='paberater-map'
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.282870996985!2d13.511988876248347!3d52.40154937203035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a907b9c7a25b01%3A0xcbb1c50d60ec9f7!2sPABerater%20GmbH!5e0!3m2!1ses!2ses!4v1704554589181!5m2!1ses!2ses"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          title="Google Map"
+          ></iframe>
+        </div>
+      </section>
     </main>
   );
 };
