284a58c583a8e9c27c1f57663e12e26c6814391a
Jassed <jassedgmartinez@gmail.com>
2024-02-29 19:31:46 +0100

    feat(successStories)added-new-storie

--- src/pages/successStories/SuccesStories.jsx
+++ src/pages/successStories/SuccesStories.jsx
@@ -4,7 +4,7 @@ import Jorge3 from "../../assets/img/jorge-5.png";
 import QAaccordion from "../../components/q&a-accordion/Q&A-accordion";
 import ScrollToTop from "../../components/scrollToTop/ScrollToTop";
 import { Link } from "react-router-dom";
-import "./SuccesStories.css"
+import "./SuccesStories.css";
 
 function SuccesStories() {
   const TranslateQuestions = [
@@ -31,77 +31,108 @@ function SuccesStories() {
     <main>
       <ScrollToTop />
       <section className="translate_services3">
-      <img src={Service3} alt="service3" className="banner-img_3" />
+        <img src={Service3} alt="service3" className="banner-img_3" />
         <div className="figure_title3">
-          <h1 className="title3_img_translate">
-            Casos de éxito
-          </h1>
+          <h1 className="title3_img_translate">Casos de éxito</h1>
         </div>
       </section>
-        <br />
-        <p className="title_text_translate">
-        Nuestros asesorados han tenido la oportunidad de contar sus vivencias, destacando los resultados positivos obtenidos y el impacto que ha tenido nuestra colaboración en su desarrollo. Sus historias son una prueba fehaciente de cómo hemos logrado acompañarlos en su crecimiento y alcanzar juntos los objetivos planteados.
-        </p>
-        <br />
+      <br />
+      <p className="title_text_translate">
+        Nuestros asesorados han tenido la oportunidad de contar sus vivencias,
+        destacando los resultados positivos obtenidos y el impacto que ha tenido
+        nuestra colaboración en su desarrollo. Sus historias son una prueba
+        fehaciente de cómo hemos logrado acompañarlos en su crecimiento y
+        alcanzar juntos los objetivos planteados.
+      </p>
+      <br />
 
       <section className="ServiceSuccessYt">
-      <div className="firstArticleTranslationSuccesStorie">
+        <div className="firstArticleTranslationSuccesStorie">
           <div className="name_video_succesStories">
             <p>
-            Reinaldo Díaz Lugo, Ingeniero Informático venezolano
-              en Alemania.
+              Reinaldo Díaz Lugo, Ingeniero Informático venezolano en Alemania.
             </p>
           </div>
           <br />
           <p className="descriptionSuccessStories">
-          Ambos realizaron el trámite de homologación profesional con PABerater, 
-          y en esta entrevista él nos cuenta cómo fue le proceso de migración, 
-          cómo es vivir en Alemania y lo que más le llamó la atención de su trabajo.
+            Ambos realizaron el trámite de homologación profesional con
+            PABerater, y en esta entrevista él nos cuenta cómo fue le proceso de
+            migración, cómo es vivir en Alemania y lo que más le llamó la
+            atención de su trabajo.
           </p>
         </div>
         <div className="divVideoSuccessStories">
-        <iframe
-          width="100%"
-          height="200"
-          src={`https://www.youtube.com/embed/VDkrGDumcCw`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          className="yt-service"
-        ></iframe>
+          <iframe
+            width="100%"
+            height="200"
+            src={`https://www.youtube.com/embed/VDkrGDumcCw`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            className="yt-service"
+          ></iframe>
         </div>
       </section>
       <br />
       <section className="ServiceSuccessYt">
-      <div className="firstArticleTranslationSuccesStorie">
-        <div className="name_video_succesStories">
-          <p>
-            Eugenio Etcheverry, <br />ingeniero electromecánico argentino
-            en Alemania.
+        <div className="firstArticleTranslationSuccesStorie">
+          <div className="name_video_succesStories">
+            <p>Verónica Anzola , Médica venezolana que ya está en Alemania </p>
+          </div>
+          <br />
+          <p className="descriptionSuccessStories">
+            Junto a su esposo
+            Reinaldo, Ingeniero Informático, y la mascota de ambos, Simón. Los
+            dos realizaron su trámite de reconocimiento profesional con
+            PABerater. En este primer video, Verónica nos cuenta cómo vivió ella
+            todo el proceso de migración, las diferencias con Latinoamérica y
+            cómo se siente en Alemania.
           </p>
         </div>
-        <br />
-        <p className="descriptionSuccessStories">
-          Realizó con PABerater el trámite de reconocimiento de su título
-          universitario, así como todos los trámites posteriores, por lo que ya
-          disfruta de estar en Alemania trabajando en su profesión y junto a su
-          familia.
+        <div className="divVideoSuccessStories">
+          <iframe
+            width="100%"
+            height="200"
+            src={`https://www.youtube.com/embed/4zUybTbOs4A`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            className="yt-service"
+          ></iframe>
+        </div>
+      </section>
+      <br />
+      <section className="ServiceSuccessYt">
+        <div className="firstArticleTranslationSuccesStorie">
+          <div className="name_video_succesStories">
+            <p>
+              Eugenio Etcheverry, <br />
+              ingeniero electromecánico argentino en Alemania.
+            </p>
+          </div>
           <br />
-        </p>
-      </div>
-      <div className="divVideoSuccessStories">
-      <iframe
-          width="100%"
-          height="200"
-          src={`https://www.youtube.com/embed/VELeEFp3Tj8`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          className="yt-service"
-        ></iframe>
-      </div>
+          <p className="descriptionSuccessStories">
+            Realizó con PABerater el trámite de reconocimiento de su título
+            universitario, así como todos los trámites posteriores, por lo que
+            ya disfruta de estar en Alemania trabajando en su profesión y junto
+            a su familia.
+            <br />
+          </p>
+        </div>
+        <div className="divVideoSuccessStories">
+          <iframe
+            width="100%"
+            height="200"
+            src={`https://www.youtube.com/embed/VELeEFp3Tj8`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            className="yt-service"
+          ></iframe>
+        </div>
       </section>
       <br />
       <section className="ServiceSuccessYt">
@@ -138,10 +169,7 @@ function SuccesStories() {
       <section className="ServiceSuccessYt">
         <div className="firstArticleTranslationSuccesStorie">
           <div className="name_video_succesStories">
-            <p>
-              Darling Gonzáles médica venezolana
-              en Alemania
-            </p>
+            <p>Darling Gonzáles médica venezolana en Alemania</p>
           </div>
           <br />
           <p className="descriptionSuccessStories">
@@ -151,76 +179,73 @@ function SuccesStories() {
           <br />
         </div>
         <div className="divVideoSuccessStories">
-        <iframe
-          width="100%"
-          height="200em"
-          src={`https://www.youtube.com/embed/UGLXVXbxNXY`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          className="yt-service"
-        ></iframe>
+          <iframe
+            width="100%"
+            height="200em"
+            src={`https://www.youtube.com/embed/UGLXVXbxNXY`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            className="yt-service"
+          ></iframe>
         </div>
       </section>
 
       <section className="callToAction_translate_container">
         <h2 className="calltoaction_translate_title">
-          <strong className="blue_name">PABerater, </strong> 
-          tu compañero ideal para transformar tu vida profesional. 
+          <strong className="blue_name">PABerater, </strong>
+          tu compañero ideal para transformar tu vida profesional.
         </h2>
         <div className="">
-        <img src={Jorge3} alt="Jorge" className="jorge3_img" />
-        <div className="text_translate_container">
+          <img src={Jorge3} alt="Jorge" className="jorge3_img" />
+          <div className="text_translate_container">
             <br />
             <Link to="/contacto">
               <button className="btn-comenzamos3">¿COMENZAMOS?</button>
             </Link>
-        </div>
-
+          </div>
         </div>
       </section>
 
       <section className="ourServicesText_container3">
-      <h2 className="Title_ourServicesText3">
+        <h2 className="Title_ourServicesText3">
           Nuestro Servicio de traducción incluye
-          </h2>
-          <ul className="li_ourServicesText3">
-            <li>
-            Traducción de su currículo y carta de presentación al alemán.
-            </li>
-            <br />
-            <li>
-            Elaboración de su carpeta de solicitud completa.
-            </li>
-            <br />
-            <li>
-            Asesoría en su búsqueda de empleo.
-            </li>
-          </ul>
-          <p className="resaltar_text3">
-          Este servicio está incluido cuando realiza su proceso de reconocimiento 
-          profesional con nosotros.
-          <br />Nos enfocamos en traducir su currículo y carta 
-          de presentación; así como elaborar su perfil profesional para que pueda 
-          postularse con éxito a ofertas de trabajo en el país.
-          </p>
+        </h2>
+        <ul className="li_ourServicesText3">
+          <li>Traducción de su currículo y carta de presentación al alemán.</li>
           <br />
-      </section>      
+          <li>Elaboración de su carpeta de solicitud completa.</li>
+          <br />
+          <li>Asesoría en su búsqueda de empleo.</li>
+        </ul>
+        <p className="resaltar_text3">
+          Este servicio está incluido cuando realiza su proceso de
+          reconocimiento profesional con nosotros.
+          <br />
+          Nos enfocamos en traducir su currículo y carta de presentación; así
+          como elaborar su perfil profesional para que pueda postularse con
+          éxito a ofertas de trabajo en el país.
+        </p>
+        <br />
+      </section>
 
       <section className="callToAction_translate_container3">
         <h3 className="text_callToAction_translate_container3">
-        ¡Vivir y trabajar como profesional en Alemania es posible 
-        de la mano de PABerater!
+          ¡Vivir y trabajar como profesional en Alemania es posible de la mano
+          de PABerater!
         </h3>
         <Link to="/contacto">
-          <button className="btn-contact_3"><p>CONTACTAR</p></button>
+          <button className="btn-contact_3">
+            <p>CONTACTAR</p>
+          </button>
         </Link>
-
       </section>
 
       <section className="questions_translate_container">
-        <h2 className="title_questions_translate_container">Preguntas Frecuentes</h2>
+        <h2 className="title_questions_translate_container">
+          Preguntas Frecuentes
+        </h2>
         <QAaccordion items={TranslateQuestions} />
       </section>
     </main>
