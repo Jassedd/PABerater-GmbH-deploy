34d804546784a255e36e05a8e67d9479156a6e76
Jassed <jassedgmartinez@gmail.com>
2024-06-20 20:26:25 +0200

    fix(UsersForm, Pagination,UsersDescription)modify & added info

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import "./Pagination.css"
+import React, { useEffect } from 'react';
+import "./Pagination.css";
 
-const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-
+const Pagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }) => {
   useEffect(() => {
     onPageChange(currentPage);
   }, [currentPage, onPageChange]);
@@ -17,10 +15,8 @@ const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
 
     const pagesToShow = [];
 
-    // Mostrar las 3 primeras páginas
     pagesToShow.push(1, 2, 3);
 
-    // Mostrar las páginas intermedias y la última página
     const startPage = Math.max(currentPage - 1, 4);
     const endPage = Math.min(currentPage + 1, totalPages - 1);
 
@@ -28,20 +24,21 @@ const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
       pagesToShow.push(i);
     }
 
-    // Mostrar la última página
     pagesToShow.push(totalPages);
 
     return pagesToShow;
   };
 
   const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
+    if (pageNumber !== currentPage) {
+      onPageChange(pageNumber);
+    }
     window.scrollTo(0, 0);
   };
 
   const handlePrevPage = () => {
     if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
+      onPageChange(currentPage - 1);
     }
     window.scrollTo(0, 0);
   };
@@ -49,7 +46,7 @@ const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
   const handleNextPage = () => {
     const totalPages = Math.ceil(totalItems / itemsPerPage);
     if (currentPage < totalPages) {
-      setCurrentPage((prevPage) => prevPage + 1);
+      onPageChange(currentPage + 1);
     }
     window.scrollTo(0, 0);
   };
@@ -78,4 +75,3 @@ const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
 };
 
 export default Pagination;
-``
